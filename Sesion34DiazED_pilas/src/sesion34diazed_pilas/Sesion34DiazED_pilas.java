eb1fc19c55b6c50bfcf1ec78f083309642576e56
rous0307 <isela0307.diaz@gmail.com>
2024-10-28 15:36:17 -0600

    Confirmacion 1 OK Diaz
    Datos desarrollo: Rosa Isela Diaz

---
+++ Sesion34DiazED_pilas/src/sesion34diazed_pilas/Sesion34DiazED_pilas.java
@@ -0,0 +1,26 @@
+
+package sesion34diazed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+public class Sesion34DiazED_pilas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //declaracion de pila
+        Stack<Integer> pilaDiaz = new Stack<>();
+        
+        pilaDiaz.push(100);
+        pilaDiaz.push(150);
+        pilaDiaz.push(230);
+        pilaDiaz.push(10);
+        pilaDiaz.push(160);
+        
+        JOptionPane.showMessageDialog(null, "Datos de pila ordenada es :\n"+pilaDiaz);//mando a llamar a mi pila
+    }
+    
+}
